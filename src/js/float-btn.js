ffe96bfbd290debe0cd4fc6facea2fbd1be25e7f
Aleksey_Filippov <mr.rashev@bk.ru>
2022-07-01 19:51:20 +0300

    fix btn 1.2

--- src/js/float-btn.js
+++ src/js/float-btn.js
@@ -1,15 +1,17 @@
-let floatFn = function(classBlock, classBtn) {
-    const block = document.querySelector(`.${classBlock}`);
-    const btn = document.querySelector(`.${classBtn}`);
+const addListenerFn = function(classBlock) {
+    const block = document.querySelector(`.${classBlock}__inner`);
+    const btn = document.querySelector(`.${classBlock}__float-btn`);
     btn.addEventListener('click', function() {
-    block.classList.toggle(`${classBlock}--hide`);
-    block.classList.toggle(`${classBlock}--show`);
-    btn.classList.toggle(`${classBtn}--hide`);
-    btn.classList.toggle(`${classBtn}--show`);
+    block.classList.toggle(`${classBlock}__inner--hide`);
+    block.classList.toggle(`${classBlock}__inner--show`);
+    btn.classList.toggle(`${classBlock}__float-btn--hide`);
+    btn.classList.toggle(`${classBlock}__float-btn--show`);
 });
 };
 
-floatFn('info-read__text', 'info-read__btn');
-floatFn('brands__list', 'brands__float-btn');
-floatFn('technical__list', 'technical__float-btn');
-
+const btnArr = document.querySelectorAll('.btn');
+for (let btnElem of btnArr) {
+    if (btnElem.classList.contains('info-read__float-btn')) addListenerFn('info-read');
+    if (btnElem.classList.contains('brands__float-btn')) addListenerFn('brands');
+    if (btnElem.classList.contains('technical__float-btn')) addListenerFn('technical');
+}
